d03b7296763e56de1989568a8e1b636ff63f4c7c
Máté Bukovácz <mate.bukovacz@stud.uni-corvinus.hu>
2021-05-09 20:34:13 +0200

    a teszt oldal lesz a főoldal

--- HajosTeszt/HajosTeszt/Startup.cs
+++ HajosTeszt/HajosTeszt/Startup.cs
@@ -33,15 +33,18 @@ namespace HajosTeszt
             {
                 app.UseDeveloperExceptionPage();
             }
-           /* else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }*/
-
+            /* else
+             {
+                 app.UseExceptionHandler("/Error");
+                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                 app.UseHsts();
+             }*/
+            DefaultFilesOptions options = new DefaultFilesOptions();
+            options.DefaultFileNames.Clear();
+            options.DefaultFileNames.Add("hajo.html");
+            
             app.UseHttpsRedirection();
-            app.UseDefaultFiles();
+            app.UseDefaultFiles(options);
             app.UseStaticFiles();
 
             app.UseRouting();
